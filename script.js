4a46aee3375734bfec746ac11a70e656eac9c843
gabriel-marrone <therealfeelz69@gmail.com>
2024-04-18 21:21:48 -0300

    commit aula javascript

--- script.js
+++ script.js
@@ -47,7 +47,6 @@ for(var x = 0; x < 5; x++){
 
 //x vai até 5 por conta que var não é restringido enquanto o i não vai até 5 por conta que o let é.
 */
-
 /*
 var VariavelDespassagem = 3
 var VariavelDespassagemJR = 6
@@ -60,7 +59,6 @@ function somamaneira(){
 somamaneira()
 console.log(xOut)
 */
-
 /*
 function ChamadentroImprimefora(){
     var x
@@ -89,7 +87,7 @@ const arrowFunction = (a, b, c) => {
 
 console.log(arrowFunction(1, 1, 6))
 */
-
+/*
 const SomaDoida = (a, b, c) => {
     return a + b + c
 }
@@ -115,3 +113,52 @@ console.log(SubtracaoBiruta(6, 3, 2))
 console.log(DivisaoDoDjanho(36, 6, 2))
 console.log(MultiplacacaoBagual(1, 3, 5))
 console.log(ModInaiaVeia(36, 6, 2))
+*/
+/*
+let array1 = [10, 20, 30, 40, 50, 100]
+
+function ImprimeNumero(array1){
+    console.log(array1)
+}
+
+array1.forEach((ImprimeNumero) => {
+    console.log(ImprimeNumero)
+});
+*/
+/*
+let listanomes = ["carlos", "cleber", "joao"]
+
+listanomes.forEach((nomeatual, indice) => {
+    console.log(`nome: ${nomeatual} esta na posicao ${indice}`)
+})
+*/
+/*
+let salarioAnual = 0
+
+let MESES = [1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400]
+
+MESES.forEach((salarioAtual)=>{
+    salarioAnual += salarioAtual
+})
+
+console.log(salarioAnual)
+*/
+
+let pessoa = {
+        nome: "Gabriel",
+        idade: 18,
+        endereco: {
+            rua: "Rua Ibitinga",
+            numero: 579,
+            bairro: "KLP"
+        },
+        academico: {
+            curso: "Engenharia de Software",
+            periodo: 2,
+            turno: "Noturno"
+        }
+}
+
+function imprimepessoa(){
+
+}
